9f177609b9a7935e14c3f1f1995d3a97e5ad1a7f
AzielHC04 <Aziel.Hidalgo110@gmail.com>
2025-10-02 09:44:19 -0600

    blablabla

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
-
+ /* recentProducts = [
+    { name: 'Crema de noche', price: 318, image: 'assets/producto1.jpg' },
+    { name: 'Labial de brillo', price: 320, image: 'assets/producto2.jpg' },
+    { name: 'Crema de peinado', price: 89, image: 'assets/producto3.jpg' },
+    { name: 'Perfume Grance', price: 320, image: 'assets/producto4.jpg' },
+    { name: 'Rocío corporal', price: 320, image: 'assets/producto5.jpg' },
+    { name: 'Mascarilla carbón', price: 89, image: 'assets/producto6.jpg' },
+    { name: 'Producto 7', price: 150, image: 'assets/producto7.jpg' },
+    { name: 'Producto 8', price: 200, image: 'assets/producto8.jpg' },
+    { name: 'Producto 9', price: 250, image: 'assets/producto9.jpg' }
+  ];  */
 }
