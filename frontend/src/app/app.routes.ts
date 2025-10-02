f8ade16388377382e97eebf848e9837cdd644f60
Katia Uc <katiaucdzib@gmail.com>
2025-10-02 11:47:55 -0600

    xs

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: '**', redirectTo: '' } // opcional, para rutas desconocidas
+  { path: '**', redirectTo: '' } 
 ];
