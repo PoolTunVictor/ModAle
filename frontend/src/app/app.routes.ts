00c02b661af1c292de397545a829efde67b0f828
Katia Uc <katiaucdzib@gmail.com>
2025-10-02 14:34:35 -0600

    Cepesue

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProductCatalogComponent } from './pages/product-catalog/product-catalog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ProductCatalogComponent},
+    {path: '**', redirectTo:''}
+];
