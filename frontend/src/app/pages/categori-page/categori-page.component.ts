5994fa359387e80767425db864a9be7f96e5bc50
Katia Uc <katiaucdzib@gmail.com>
2025-10-12 18:41:49 -0600

    Se realizaron cambios al catalogo y se le dio funcionalidad al rango de precios

--- frontend/src/app/pages/categori-page/categori-page.component.ts
+++ frontend/src/app/pages/categori-page/categori-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router'; // 🔹 Importa Router
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductosService } from '../../core/service/productos.service';
 import { CommonModule } from '@angular/common';
 
@@ -13,11 +13,13 @@ import { CommonModule } from '@angular/common';
 export class CategoriPageComponent implements OnInit {
   nombreCategoria: string = '';
   productos: any[] = [];
+  productosCompletos: any[] = [];
+  precioMax: number = 15; 
 
   constructor(
     private route: ActivatedRoute,
     private productosService: ProductosService,
-    private router: Router // 🔹 Inyecta Router
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -27,15 +29,27 @@ export class CategoriPageComponent implements OnInit {
 
   cargarProductos(nombre: string) {
     this.productosService.getProductosPorCategoria(nombre)
-      .subscribe(productos => this.productos = productos);
+      .subscribe(productos => {
+        this.productosCompletos = productos || [];
+        this.productos = [...this.productosCompletos];
+      });
   }
 
   agregarACesta(producto: any) {
     console.log('Añadido al carrito:', producto.nombre);
   }
 
-  // 🔹 Método para regresar al home
   regresarHome() {
     this.router.navigate(['/home-page']);
+  }
+
+  // 🔹 Filtrar por precio máximo
+   filtrarPorPrecio(event: any) {
+    this.precioMax = +event.target.value; // convertir a número
+    if (this.productosCompletos && this.productosCompletos.length > 0) {
+      this.productos = this.productosCompletos.filter(p => p.precio <= this.precioMax);
+    } else {
+      this.productos = [];
+    }
   }
 }
