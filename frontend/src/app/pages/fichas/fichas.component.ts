a11c5ac7e756002f7b5fe22c33ec8417a2cda5c4
Katia Uc <katiaucdzib@gmail.com>
2025-10-17 11:43:30 -0600

    Reparacion de la pagina de fichas

--- frontend/src/app/pages/fichas/fichas.component.ts
+++ frontend/src/app/pages/fichas/fichas.component.ts
@@ -1,29 +1,42 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-fichas',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './fichas.component.html',
+  imports: [CommonModule],
   styleUrls: ['./fichas.component.css']
 })
 export class FichasComponent {
-  constructor(private router: Router) {}
+  selectedLocation = 'all';
 
   fichas = [
-    { nombre: 'Sophia Clark', fecha: '2024-01-15', estado: 'Nuevo', ubicacion: 'Calkiní' },
-    { nombre: 'Liam Walker', fecha: '2024-01-16', estado: 'En proceso', ubicacion: 'Dzibtaché' },
-    { nombre: 'Olivia Carter', fecha: '2024-01-17', estado: 'Nuevo', ubicacion: 'Bacabchén' },
-    { nombre: 'Noah Evans', fecha: '2024-01-18', estado: 'Completado', ubicacion: 'Calkiní' },
-    { nombre: 'Ava Bennett', fecha: '2024-01-19', estado: 'Nuevo', ubicacion: 'Calkiní' },
-    { nombre: 'Ethan Reed', fecha: '2024-01-20', estado: 'En proceso', ubicacion: 'Dzibtaché' },
-    { nombre: 'Isabella Hayes', fecha: '2024-01-21', estado: 'Nuevo', ubicacion: 'Bacabchén' },
-    { nombre: 'Jackson Powell', fecha: '2024-01-22', estado: 'Completado', ubicacion: 'Calkiní' },
+    { name: 'Sophia Clark', date: '2024-01-15', status: 'Nuevo', location: 'Calkiní' },
+    { name: 'Liam Walker', date: '2024-01-16', status: 'En proceso', location: 'Dzibalché' },
+     { name: 'Liam Walker', date: '2024-01-16', status: 'En proceso', location: 'Becal' },
+    { name: 'Olivia Carter', date: '2024-01-17', status: 'Nuevo', location: 'Bacabchén' },
+    { name: 'Noah Evans', date: '2024-01-18', status: 'Completado', location: 'Calkiní' },
+    { name: 'Ava Bennett', date: '2024-01-19', status: 'Nuevo', location: 'Calkiní' },
+    { name: 'Ethan Reed', date: '2024-01-20', status: 'En proceso', location: 'Dzibalché' },
+    { name: 'Isabella Hayes', date: '2024-01-21', status: 'Nuevo', location: 'Bacabchén' },
+    { name: 'Jackson Powell', date: '2024-01-22', status: 'Completado', location: 'Calkiní' }
   ];
 
-  verDetalle() {
-    this.router.navigate(['/fichas-detalle']);
+  fichasFiltradas = [...this.fichas];
+
+  constructor(private router: Router) {}
+
+  filtrarPorUbicacion(location: string) {
+    this.selectedLocation = location;
+    if (location === 'all') {
+      this.fichasFiltradas = [...this.fichas];
+    } else {
+      this.fichasFiltradas = this.fichas.filter(f => f.location === location);
+    }
+  }
+
+  verDetalle(ficha: any) {
+    this.router.navigate(['/fichas/:id'], { queryParams: { name: ficha.name } });
   }
 }
