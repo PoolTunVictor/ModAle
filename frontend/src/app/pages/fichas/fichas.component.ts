0e8629824c8db35429274b2ea8dfdc8694e8a6fc
Katia Uc <katiaucdzib@gmail.com>
2025-11-17 18:14:35 -0600

    Se creó el dashboard de la pagina administrador, se agregaron los filtros, y se dejaron responsivas las páginas

--- frontend/src/app/pages/fichas/fichas.component.ts
+++ frontend/src/app/pages/fichas/fichas.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FichaDetalleComponent } from './ficha-detalle/ficha-detalle.component';
+import { FormsModule } from '@angular/forms';   // ✅ IMPORTANTE
 
 @Component({
   selector: 'app-fichas',
   templateUrl: './fichas.component.html',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],  // ✅ AGREGA ESTO
   styleUrls: ['./fichas.component.css']
 })
 export class FichasComponent {
   selectedLocation = 'all';
 
+  filtroLocalidad: string = 'all';
+  filtroEstado: string = 'all';
+  filtroFecha: string = '';
+
   fichas = [
     { name: 'Sophia Clark', date: '2024-01-15', status: 'Nuevo', location: 'Calkiní' },
     { name: 'Liam Walker', date: '2024-01-16', status: 'En proceso', location: 'Dzibalché' },
-     { name: 'Liam Walker', date: '2024-01-16', status: 'En proceso', location: 'Becal' },
+    { name: 'Liam Walker', date: '2024-01-16', status: 'En proceso', location: 'Becal' },
     { name: 'Olivia Carter', date: '2024-01-17', status: 'Nuevo', location: 'Calkiní' },
     { name: 'Noah Evans', date: '2024-01-18', status: 'Completado', location: 'Calkiní' },
     { name: 'Ava Bennett', date: '2024-01-19', status: 'Nuevo', location: 'Calkiní' },
@@ -30,19 +34,29 @@ export class FichasComponent {
 
   filtrarPorUbicacion(location: string) {
     this.selectedLocation = location;
-    if (location === 'all') {
-      this.fichasFiltradas = [...this.fichas];
-    } else {
-      this.fichasFiltradas = this.fichas.filter(f => f.location === location);
-    }
+    this.filtroLocalidad = location;
+    this.aplicarFiltros();
   }
 
-fichaSeleccionada: any = null;
+  aplicarFiltros() {
+    this.fichasFiltradas = this.fichas.filter(f => {
 
-verDetalle(ficha: any) {
-  this.router.navigate(['/admin/ficha/ficha-detalle', ficha.id]);
-}
+      const coincideLocalidad =
+        this.filtroLocalidad === 'all' || f.location === this.filtroLocalidad;
 
+      const coincideEstado =
+        this.filtroEstado === 'all' || f.status === this.filtroEstado;
 
+      const coincideFecha =
+        this.filtroFecha === '' || f.date.startsWith(this.filtroFecha);
 
+      return coincideLocalidad && coincideEstado && coincideFecha;
+    });
+  }
+
+  fichaSeleccionada: any = null;
+
+  verDetalle(ficha: any) {
+    this.router.navigate(['/admin/ficha/ficha-detalle', ficha.id]);
+  }
 }
