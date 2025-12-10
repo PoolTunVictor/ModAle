a21312f15be6cb76f1de69a005f9a32625053db6
Katia Jaqueline <katiaucdzib@gmail.com>
2025-12-10 09:56:06 -0600

    diseño del frontend actualizado

--- frontend/src/app/pages/reportes/reportes.component.ts
+++ frontend/src/app/pages/reportes/reportes.component.ts
@@ -23,7 +23,7 @@ export class ReportesComponent implements OnInit {
   productosTop = [
     { nombre: 'Gloss Mágico de Sandía', categoria: 'Maquillaje', vendidos: 35, ingresos: 875 },
     { nombre: 'Pinzas para depilar', categoria: 'Accesorios', vendidos: 30, ingresos: 450 },
-    { nombre: 'HOMME Eau de Parfum', categoria: 'Perfumes', vendidos: 7, ingresos: 525 },
+    { nombre: 'Pattern Eau de Parfum', categoria: 'Perfumes', vendidos: 7, ingresos: 525 },
     { nombre: 'Salvace For Men', categoria: 'Perfumes', vendidos: 10, ingresos: 750},
     { nombre: 'Boss Orange For Men', categoria: 'Perfumes', vendidos: 7, ingresos: 525},
     { nombre: 'Mangas protectoras para el sol', categoria: 'Prendas', vendidos: 20, ingresos: 400},
--- frontend/src/app/shared/header-admin/header-admin.component.ts
+++ frontend/src/app/shared/header-admin/header-admin.component.ts
@@ -24,5 +24,6 @@ export class HeaderAdminComponent {
       this.router.navigate(['/login']);
     }
   }
+  menuAbierto = false;
 }
 
