4dee0b9581c391a09d70dd031276f66211a58a1f
Katia Jaqueline <katiaucdzib@gmail.com>
2025-12-10 02:05:14 -0600

    xd

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:4200",
         "http://127.0.0.1:4200",
-        "https://modale-production.up.railway.app"
+        "https://modale-production.up.railway.app",
         "https://modale.click",            
         "https://www.modale.click"   
     ],
